4c4ad6135b124010150588f7bad4728f82cb5a54
Serhii Kazanli <orodeljava@gmail.com>
2024-01-09 17:02:14 +0200

    Next commit

--- com.javarush.cryptoanalyzer.kazanli/src/CaesarCipher.java
+++ com.javarush.cryptoanalyzer.kazanli/src/CaesarCipher.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println("Вы находитесь в приложении \"Код Цезаря\"!");
         System.out.println("Выберите режим работы :");
@@ -12,19 +12,23 @@ public class CaesarCipher {
         System.out.println("3. Взламываем");
         System.out.println("4. Интеллектуальный взлом.(Не рекомендуется для коротких текстов)");
 
-        Scanner console = new Scanner(System.in);
-        int n = Integer.parseInt(console.nextLine());
+        try {
+            Scanner console = new Scanner(System.in);
+            int n = Integer.parseInt(console.nextLine());
 
-        if (n == 1) {
-            Encryption.doEncrypt();
-        } else if (n == 2) {
-            Decryption.doDecrypt();
-        } else if (n == 3) {
-            BruteForse.doBruteForse();
-        } else if (n == 4) {
-            IntellectualBruteForce.doIntellectualBruteForce();
-        } else {
-            System.out.println("Вы ввели не корректное число.");
+            if (n == 1) {
+                Encryption.doEncrypt();
+            } else if (n == 2) {
+                Decryption.doDecrypt();
+            } else if (n == 3) {
+                BruteForse.doBruteForse();
+            } else if (n == 4) {
+                IntellectualBruteForce.doIntellectualBruteForce();
+            } else {
+                System.out.println("Вы ввели не корректное число.");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
